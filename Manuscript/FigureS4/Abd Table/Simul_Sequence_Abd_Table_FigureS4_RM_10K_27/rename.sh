8fb6355dc8a62173e4f589504d1cddc8881da0c5
sunzhengCDNM <119537814+sunzhengCDNM@users.noreply.github.com>
2023-08-19 22:58:20 +0800

    Delete rename.sh

--- Manuscript/FigureS4/Abd Table/Simul_Sequence_Abd_Table_FigureS4_RM_10K_27/rename.sh
+++
@@ -1,7 +0,0 @@
-rename _10_ _010_ *
-rename _20_ _020_ *
-rename _50_ _050_ *
-rename taxonomic_abd Truth_Tax *.txt
-rename sequence_abd Truth_Seq *.txt
-mv *Truth_Tax.txt Tax
-mv *Truth_Seq.txt Seq
