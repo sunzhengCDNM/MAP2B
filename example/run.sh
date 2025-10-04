2fd18876a17154c6d9108d9262907bb17ce04d78
sunzhengCDNM <119537814+sunzhengCDNM@users.noreply.github.com>
2025-10-04 11:15:26 -0400

    MAP2B v1.6 update

--- example/run.sh
+++ example/run.sh
@@ -1,7 +1,7 @@
 # conda activate MAP2B.1.5
 # pre data
 mkdir -p data/ && \
-wget -t 3 -O data/shotgun_MSA-1002_1.fq.gz https://figshare.com/ndownloader/files/38346149/shotgun_MSA-1002_1.fq.gz && \
-wget -t 3 -O data/shotgun_MSA-1002_2.fq.gz https://figshare.com/ndownloader/files/38346155/shotgun_MSA-1002_2.fq.gz && \
+wget --user-agent="Mozilla/5.0" -t 3 -O data/shotgun_MSA-1002_1.fq.gz https://figshare.com/ndownloader/files/38346149/shotgun_MSA-1002_1.fq.gz && \
+wget --user-agent="Mozilla/5.0" -t 3 -O data/shotgun_MSA-1002_2.fq.gz https://figshare.com/ndownloader/files/38346155/shotgun_MSA-1002_2.fq.gz && \
 # run demo
 time python3 ../bin/MAP2B.py -i data.list
