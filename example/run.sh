a9746a412643b7bf9da599177b507fd8cd37b494
sunzhengCDNM <119537814+sunzhengCDNM@users.noreply.github.com>
2026-01-22 11:10:07 -0500

    MAP2B 1.8 update

--- example/run.sh
+++ example/run.sh
@@ -1,7 +1,9 @@
-# conda activate MAP2B.1.5
+conda activate MAP2B.1.7
 # pre data
 mkdir -p data/ && \
 wget --user-agent="Mozilla/5.0" -t 3 -O data/shotgun_MSA-1002_1.fq.gz https://figshare.com/ndownloader/files/38346149/shotgun_MSA-1002_1.fq.gz && \
 wget --user-agent="Mozilla/5.0" -t 3 -O data/shotgun_MSA-1002_2.fq.gz https://figshare.com/ndownloader/files/38346155/shotgun_MSA-1002_2.fq.gz && \
-# run demo
+# run demo if data in FASTQ format
 time python3 ../bin/MAP2B.py -i data.list
+# run demo if data in CRAM format
+# time python3 ../bin/MAP2B.py -i data.list -gm data/Homo_sapiens_assembly38.fasta
